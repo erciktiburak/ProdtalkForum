429c91ec8c39daede5eebf50f0cd66b5374ee756
Burak ERCIKTI <burakercikti@outlook.com>
2023-06-04 23:14:23 +0000

    new-commit

--- resources/views/partials/dialogs/socials.blade.php
+++ resources/views/partials/dialogs/socials.blade.php
@@ -1,8 +1,6 @@
 <div class="w-full flex flex-col justify-center items-center gap-2 mt-5">
     <div class="text-center text-sm text-slate-700 uppercase flex items-center gap-5">
-        <div class="w-[50px] h-1 border-b border-slate-300"></div>
-        Or sign up with
-        <div class="w-[50px] h-1 border-b border-slate-300"></div>
+
     </div>
     <div class="w-full flex flex-row flex-wrap justify-center items-center mt-2">
         @foreach(Socials::enabledCases() as $social)
