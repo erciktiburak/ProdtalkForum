aa50451e2e8f9e56aa922784923efcdbe2960474
Burak Erçıktı <abathvarg@gmail.com>
2023-06-21 01:30:04 +0300

    nsfw-tag-conditional

--- app/View/Components/Home/Tags.php
+++ app/View/Components/Home/Tags.php
@@ -3,12 +3,15 @@
 namespace App\View\Components\Home;
 
 use App\Models\Tag;
+use App\Models\UserRole;
 use Illuminate\View\Component;
 
 class Tags extends Component
 {
+
     public $tags;
     public $tag = null;
+      
 
     /**
      * Create a new component instance.
@@ -18,7 +21,19 @@ class Tags extends Component
     public function __construct($tag = null)
     {
         $this->tag = $tag;
-        $this->tags = Tag::all();
+
+        $userHasRole = UserRole::where('user_id', auth()->id())
+            ->where('role_id', 1) // İstenen role id'si
+            ->exists();
+    
+        if ($userHasRole) {
+            // Kullanıcının belirtilen rolü varsa, tüm tagları al
+            $this->tags = Tag::all();
+        } else {
+            // Diğer kullanıcılar için Tag id'si 11 olanı hariç al
+            $this->tags = Tag::where('id', '<>', 11)->get();
+        }
+        
     }
 
     /**
