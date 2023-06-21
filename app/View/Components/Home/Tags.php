0f24901245f02ab56685e99cba5d753d500c2484
Burak Erçıktı <abathvarg@gmail.com>
2023-06-21 19:44:31 +0300

    update

--- app/View/Components/Home/Tags.php
+++ app/View/Components/Home/Tags.php
@@ -3,15 +3,12 @@
 namespace App\View\Components\Home;
 
 use App\Models\Tag;
-use App\Models\UserRole;
 use Illuminate\View\Component;
 
 class Tags extends Component
 {
-
     public $tags;
     public $tag = null;
-      
 
     /**
      * Create a new component instance.
@@ -21,19 +18,7 @@ class Tags extends Component
     public function __construct($tag = null)
     {
         $this->tag = $tag;
-
-        $userHasRole = UserRole::where('user_id', auth()->id())
-            ->where('role_id', 1) // İstenen role id'si
-            ->exists();
-    
-        if ($userHasRole) {
-            // Kullanıcının belirtilen rolü varsa, tüm tagları al
-            $this->tags = Tag::all();
-        } else {
-            // Diğer kullanıcılar için Tag id'si 11 olanı hariç al
-            $this->tags = Tag::where('id', '<>', 11)->get();
-        }
-        
+        $this->tags = Tag::all();
     }
 
     /**
